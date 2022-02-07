8c82b1b70c6e3d817cbc63dbeb4022b6a6db0aca
Kirandhanwai <kirandhanwai123@gmail.com>
2022-02-07 09:54:03 +0530

    Added : Check Whether a Number is Even or odd

--- src/com/practiceProblem/java/Problems.java
+++ src/com/practiceProblem/java/Problems.java
@@ -5,6 +5,13 @@ import java.util.Scanner;
 public class Problems {
 
 	public static void main(String[] args) {
-		
-	}
+		int num;
+	    System.out.println("Enter number:");
+	    Scanner input = new Scanner(System.in);
+	    num = input.nextInt();
+	    if ( num % 2 == 0 )
+	        System.out.println("Entered number is even");
+	     else
+	        System.out.println("Entered number is odd");
+	  }
 }
